2ea6e9d15b3af9f16876a341bf51ddbebb5f9cf2
Riomus <riomus@gmail.com>
2015-02-22 17:04:49 +0100

    presentation texts

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-git push origin --delete gh-pages
+git add **
+git commit -m '[ci skip] GH-Pages release, before publish master commit'
+git push origin master --force
+
+
+git push origin --delete gh-pages --force
 git checkout -b gh-pages
 
 git add **
