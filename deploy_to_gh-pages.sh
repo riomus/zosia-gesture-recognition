d04339731506ea721030293c082af730df4d2927
Travis-CI <riomus@gmail.com>
2015-02-22 17:55:59 +0100

    CI final fix

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -3,7 +3,6 @@
 git push --force origin :gh-pages 
 git checkout -b gh-pages
 git add **
-git add  .**
 git commit -m '[ci skip] GH-Pages release'
 git push origin gh-pages --force
 git checkout master
