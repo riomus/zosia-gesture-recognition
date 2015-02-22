8841cc5972f8cc4dc0e4c069b0403fd32527a6f5
Riomus <riomus@gmail.com>
2015-02-22 17:09:00 +0100

    CI fix

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 git remote set-url --push origin https://github.com/riomus/zosia-gesture-recognition.git
-git add **
-git commit -m '[ci skip] GH-Pages release, before publish master commit'
-git push origin master --force
-
 
 git push origin --delete gh-pages --force
 git checkout -b gh-pages
