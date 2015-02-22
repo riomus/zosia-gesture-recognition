170b3ef649a532fc9cea7620d8c357cc11b2ead4
Travis-CI <riomus@gmail.com>
2015-02-22 17:41:22 +0100

    CI fix

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-git push --force "https:/github.com/riomus/zosia-gesture-recognition.git" --delete gh-pages 
+git push "https:/github.com/riomus/zosia-gesture-recognition.git" --delete gh-pages --force
 git checkout -b gh-pages
 
 git add **
 git commit -m '[ci skip] GH-Pages release'
-git push --force "https://github.com/riomus/zosia-gesture-recognition.git}" origin gh-pages --force
+git push "https://github.com/riomus/zosia-gesture-recognition.git" origin gh-pages --force
 git checkout master
