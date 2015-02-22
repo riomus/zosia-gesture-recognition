057d71cfbb6c32426d2569bfa1eb04d5036cb8df
Riomus <riomus@gmail.com>
2015-02-22 16:57:41 +0100

    CI fix

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+git pull origin gh-pages
 git branch -D gh-pages
 git checkout -b gh-pages
 
 git add **
 git commit -m '[ci skip] GH-Pages release'
-git push origin gh-pages
+git push origin gh-pages --force
 git checkout master
