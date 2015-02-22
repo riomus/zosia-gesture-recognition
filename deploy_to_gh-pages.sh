727711321969b74eb22ab305cba4bcd9d6078760
Travis-CI <riomus@gmail.com>
2015-02-22 17:35:38 +0100

    CI fix

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git config --global user.email "riomus@gmail.com"
+git config --global user.name "Travis-CI"
 git push --force "https://${GH_TOKEN}@github.com/riomus/zosia-gesture-recognition.git" --delete gh-pages 
 git checkout -b gh-pages
 
