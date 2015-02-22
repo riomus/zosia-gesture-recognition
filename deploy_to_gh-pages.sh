bcc4cea8d5ca266c07fa9f1618f85015f8f1fe99
Travis-CI <riomus@gmail.com>
2015-02-22 17:27:10 +0100

    CI fix

--- deploy_to_gh-pages.sh
+++ deploy_to_gh-pages.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-git config --global user.email "riomus@gmail.com"
-git config --global user.name "Travis-CI"
-cat $GH_TOKEN > deploy_key.pem
-chmod 600 deploy_key.pem
-ssh-add deploy_key.pem
-git remote set-url --push origin https://github.com/riomus/zosia-gesture-recognition.git
-
-git push origin --delete gh-pages --force
+git push --force "https://${GH_TOKEN}@${GH_REF}" --delete gh-pages 
 git checkout -b gh-pages
 
 git add **
 git commit -m '[ci skip] GH-Pages release'
-git push origin gh-pages --force
+git push --force "https://${GH_TOKEN}@${GH_REF}" origin gh-pages --force
 git checkout master
